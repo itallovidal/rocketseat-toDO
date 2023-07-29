9fb4e8469583a2a393533900267f2165787d9b08
italloVidal <itallovidalll@gmail.com>
2023-07-29 01:36:56 -0300

    msg de erro

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -3,15 +3,17 @@ import React, {ChangeEvent, FormEvent} from "react";
 import {TaskInterface} from "../TaskList/Interfaces.tsx";
 
 interface FormProps {
-    setTaskList: React.Dispatch<React.SetStateAction<TaskInterface[]>>
+    setTaskList: React.Dispatch<React.SetStateAction<TaskInterface[]>>,
 }
 
 function Form({setTaskList}: FormProps) {
     const [task, setTask] = React.useState<string>('')
+    const [error, setError] = React.useState<boolean>(false)
 
     function submitTask(e: FormEvent){
         e.preventDefault()
         if(task.length > 3){
+            setError(false)
             setTaskList((prevTasks)=> {
                 return [...prevTasks, {
                     id: prevTasks.length + 1,
@@ -19,9 +21,11 @@ function Form({setTaskList}: FormProps) {
                     task: task
                 }]
             })
+            setTask('')
+        }else{
+            setError(true)
         }
 
-        setTask('')
     }
 
     function handleChange({target} : ChangeEvent<HTMLInputElement>){
@@ -31,6 +35,7 @@ function Form({setTaskList}: FormProps) {
     return (
         <form onSubmit={submitTask} className={styles.wrapper}>
             <input onChange={handleChange} value={task} className={styles.inputStyle} placeholder={'Adicione uma nova tarefa..'}/>
+            {error && <p className={styles.errorMsg}>Ops, a tarefa precisa de mais de 3 caracteres!</p>}
             <button className={styles.btnStyle}> Criar
                 <i className="fa-solid fa-plus"></i>
             </button>
